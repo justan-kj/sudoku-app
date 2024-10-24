69451fde4ab492ffee41098311361333c8abaed0
Justin Tan <jtjtjt703@gmail.com>
2024-10-24 16:59:06 +0100

    changed button layout

--- frontend/sudoku-frontend/src/components/Controls.js
+++ frontend/sudoku-frontend/src/components/Controls.js
@@ -11,55 +11,61 @@ function Controls({ onPress, onHotkeyPress, fillMode, toggle }) {
   }, [onHotkeyPress]);
 
   return (
-    <div>
-      <div className="container d-flex-inline justify-content-center flex-column">
-        <div className="container d-flex-inline row-gap-1">
-          <Button onClick={() => onPress(1)} className="m-1">
-            1
-          </Button>
-          <Button onClick={() => onPress(2)} className="m-1">
-            2
-          </Button>
-          <Button onClick={() => onPress(3)} className="m-1">
-            3
-          </Button>
-          <Button onClick={() => onPress(4)} className="m-1">
-            4
-          </Button>
-          <Button onClick={() => onPress(5)} className="m-1">
-            5
-          </Button>
-          <ToggleButton
-            id="toggle-check"
-            type="checkbox"
-            variant="secondary"
-            checked={fillMode}
-            value="1"
-            onChange={(e) => toggle(e.currentTarget.checked)}
-          >
-            <i className="bi bi-pencil"></i>
-          </ToggleButton>
-        </div>
-        <div className="container d-flex-inline gap-1">
-          <Button onClick={() => onPress(6)} className="m-1">
-            6
-          </Button>
-          <Button onClick={() => onPress(7)} className="m-1">
-            7
-          </Button>
-          <Button onClick={() => onPress(8)} className="m-1">
-            8
-          </Button>
-          <Button onClick={() => onPress(9)} className="m-1">
-            9
-          </Button>
-          <Button onClick={() => onPress(0)} className="m-1">
-            <i className="bi bi-eraser"></i>
-          </Button>
-          <Button onClick={() => onPress("check")} className="m-1">
-            <i className="bi bi-check2-circle"></i>
-          </Button>
-        </div>
+    <div className="container w-75">
+      <div className="row justify-content-center">
+        <Button onClick={() => onPress(1)} className="m-1 col-1">
+          1
+        </Button>
+        <Button onClick={() => onPress(2)} className="m-1 col-1">
+          2
+        </Button>
+        <Button onClick={() => onPress(3)} className="m-1 col-1">
+          3
+        </Button>
+        <ToggleButton
+          id="toggle-check"
+          type="checkbox"
+          variant="secondary"
+          checked={fillMode}
+          value="1"
+          onChange={(e) => toggle(e.currentTarget.checked)}
+          style={{
+            backgroundColor: fillMode ? "#0d6efd" : "#6c757d",
+            color: "white",
+          }}
+          className="m-1 col-1"
+        >
+          <i className="bi bi-pencil"></i>
+        </ToggleButton>
+      </div>
+      <div className="row justify-content-center">
+        <Button onClick={() => onPress(4)} className="m-1 col-1">
+          4
+        </Button>
+        <Button onClick={() => onPress(5)} className="m-1 col-1">
+          5
+        </Button>
+        <Button onClick={() => onPress(6)} className="m-1 col-1">
+          6
+        </Button>
+        <Button onClick={() => onPress(0)} className="m-1 col-1">
+          <i className="bi bi-eraser"></i>
+        </Button>
+      </div>
+      <div className="row  justify-content-center">
+        <Button onClick={() => onPress(7)} className="m-1 col-1">
+          7
+        </Button>
+        <Button onClick={() => onPress(8)} className="m-1 col-1">
+          8
+        </Button>
+        <Button onClick={() => onPress(9)} className="m-1 col-1">
+          9
+        </Button>
+
+        <Button onClick={() => onPress("check")} className="m-1 col-1">
+          <i className="bi bi-check2-circle"></i>
+        </Button>
       </div>
     </div>
   );
